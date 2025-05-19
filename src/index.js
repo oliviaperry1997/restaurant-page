0450beee8ad9775093c926c947617c45d1657f93
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-05-19 17:05:12 +0100

    Add main logo
    This commit adds the logo to the home page, with an h1 and an imported
    image.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+import salmonLogo from "./assets/freepik__a_salmon_smoking_a_joint.png";
+
+const logo = document.createElement("img");
+logo.src = salmonLogo;
+
+document.querySelector("#logo").appendChild(logo);
