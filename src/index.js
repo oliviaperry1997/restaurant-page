fc876c887c2a175a650d9d17ecf0a02b78f2fb25
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-05-20 12:12:00 +0100

    Complete main page content and styling
    This commit adds all the content for the main page and styles it. The
    page includes a logo, a description, a section displaying hours, and a
    section that shows a made up location for the business.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 import salmonLogo from "./assets/freepik__a_salmon_smoking_a_joint.png";
+import cascadianFlag from "./assets/Cascadian-Flag-1024x614.png";
 
 const logo = document.createElement("img");
 logo.src = salmonLogo;
-
 document.querySelector("#logo").appendChild(logo);
+
+const flag = document.createElement("img");
+flag.src = cascadianFlag;
+document.querySelector(".location").appendChild(flag);
